3d8f31ec3e3a2071642e02011f7e130176653950
ojrv <ojrv.uy@gmail.com>
2025-09-27 10:57:49 -0300

    Update registro.js

--- registro.js
+++ registro.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
   const client = window.supabase.createClient(
-    'https://TU_PROYECTO.supabase.co', // ← Reemplazá con tu URL real
-    'TU_CLAVE_PUBLICA'                 // ← Reemplazá con tu clave anon
+    'https://fdlnptxefjzoxeiqsujx.supabase.co', // ← Reemplazá con tu URL real
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkbG5wdHhlZmp6b3hlaXFzdWp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NDE3ODIsImV4cCI6MjA3NDQxNzc4Mn0.BI9M80Fr-AevXnHBATTJZkRjFrCGn4x7cgDPkTinNms'                 // ← Reemplazá con tu clave anon
   );
 
   const form = document.getElementById('formUsuario');
@@ -31,4 +31,5 @@ window.addEventListener('load', () => {
       alert("Error inesperado: " + err.message);
     }
   });
+
 });
